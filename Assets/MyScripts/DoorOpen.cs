dda57e6bf5c4b6e9acf3521693f356f4146d9b0e
yhuen24 <56414768+yhuen24@users.noreply.github.com>
2020-09-02 14:07:32 +0100

    upload test

---
+++ Assets/MyScripts/DoorOpen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("open");
+            animator.SetBool("near", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if(col.CompareTag("Player"))
+        {
+            Debug.Log("close");
+            animator.SetBool("near", false);
+        }
+    }
+
+}
